dd3d75a52bf648863052ea2f639032d0046162dd
nickyreinert <mail@nickyreinert.de>
2017-11-17 13:38:24 +0100

    adding two columns to output
    - first new column now contains speed in seconds
    - second new column now contains speed in MBit / second
    - cleaning up last column (1MByte, 10MByte and so on)
    - clean up loop

--- speedTest.sh
+++ speedTest.sh
@@ -2,7 +2,7 @@
 # follow the google date format convetion, otherwise the date will not be recognizied:
 # https://developers.google.com/datastudio/connector/semantics
 #sourceFiles=("1Mb.db" "10Mb.db" "100Mb.db" "1Gb.db")
-sourceFiles=("1Mb.db" "10Mb.db" "100Mb.db" "1Gb.db")
+sourceFileSizes=(1 10 100 1000)
 sourceBaseUrl="http://speedtest.ftp.otenet.gr/files/test"
 destinationFolder="/share/Download/Speedtest/"
 journalFile="/share/Download/Speedtest/results.csv"
@@ -10,18 +10,35 @@ pause=3
 loops=3
 i=0
 
-for sourceFile in "${sourceFiles[@]}";
+for sourceFileSize in "${sourceFileSizes[@]}";
 do
 
 	while [ $i -lt $loops ]; do
 
 		startTime=$(($(date +%s%N)))
-		wget --quiet --output-document=$destinationFolder$sourceFile.tmp $sourceBaseUrl$sourceFile
-		endTime=$(($(date +%s%N)))
-		delay=$(($endTime - $startTime))
- 		echo "$(date '+%Y-%m-%d %H:%M:%S'),$delay,$sourceFile" >> $journalFile
 
-		if [ "$sourceFile" = "1Gb.db" ]
+		if [ "$sourceFileSize" = "1000" ]
+		then
+
+			wget --quiet --output-document=$destinationFolder$sourceFileSize"Mb.db.tmp" $sourceBaseUrl"1Gb.db"
+
+		else
+
+			wget --quiet --output-document=$destinationFolder$sourceFileSize"Mb.db.tmp" $sourceBaseUrl$sourceFileSize"Mb.db"
+
+		fi
+
+		endTime=$(($(date +%s%N)))
+		delayNsecs=$(($endTime - $startTime))
+
+		delaySecs=$(awk -v delayNsecs="$delayNsecs" 'BEGIN{printf "%.4f\n", ( delayNsecs / 1000000000)}')
+
+		MBitPerSec=$(awk -v sourceFileSize=$sourceFileSize -v delayMSecs=$delaySecs 'BEGIN{printf "%.4f\n", ( sourceFileSize * 8 / delayMSecs)}')
+
+		echo "$(date '+%Y-%m-%d %H:%M:%S'),$delayNsecs,$delaySecs,$MBitPerSec,$sourceFileSize"MByte"" >> $journalFile
+
+
+		if [ "$sourceFileSize" = "1000" ]
 		then
 			exit
 		fi
